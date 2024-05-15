52dac794877ad256c366d7a61ecc674619f0d169
Ali Athary <101128997+Ali-Athary@users.noreply.github.com>
2024-05-15 20:41:31 +0330

    pptable syscall define (#1)
    * pptable syscall define
    
    * part 1

---
+++ mytest.c
@@ -0,0 +1,11 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main(void) 
+{
+    printf(0, "--- start pptable ---\n");
+    ps();
+    printf(0, "--- end pptable ---\n");
+    exit();
+} 
--- sysproc.c
+++ sysproc.c
@@ -89,3 +89,10 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+int 
+sys_ps(void)
+{
+  ps();
+  return 0;
+}
