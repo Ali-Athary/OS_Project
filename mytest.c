cc58ca053c14b1d8ab78de939bf9db55f00790c7
Ali-Athary <ali.athary.ce@gmail.com>
2024-05-17 23:58:09 +0330

    test one state

--- mytest.c
+++ mytest.c
@@ -3,14 +3,27 @@
 #include "user.h"
 #include "process_info_t.h"
 
+#define N  20
+
 int main(void) 
 {
-    struct process_info_t *my_struct;
-    my_struct = malloc(sizeof(my_struct));
-    
-    printf(0, "--- start pptable ---\n");
-    ps(1, 12, my_struct);
-    printf(0, "--- end pptable ---\n");
-    printf(1, "my_struct->pid: %d\n", my_struct->pid);
+    int n, pid;
+    for(n=0; n<N; n++){
+        pid = fork();
+        if(pid < 0)
+        break;
+        if(pid == 0)
+        exit();
+    }
+
+    if (pid != 0){
+        struct process_info_t *my_struct;
+        my_struct = malloc(sizeof(my_struct));
+
+        ps(4, 2, my_struct);
+        printf(1, "my_struct->name: %s\n", my_struct->name);
+        printf(1, "my_struct->pid: %d\n", my_struct->pid);
+        printf(1, "my_struct->state: %d\n", my_struct->state);
+    }
     exit();
 } 
